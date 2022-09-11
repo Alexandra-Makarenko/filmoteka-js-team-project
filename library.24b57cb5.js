2d43e9dff3e3367b5306f75e9af3a3d0ed4b1fe0
Alexandra-Makarenko <37274881+Alexandra-Makarenko@users.noreply.github.com>
2022-09-11 12:18:34 +0000

    Deploying to gh-pages from @ Alexandra-Makarenko/filmoteka-js-team-project@fcd52ffde2efb9bf816c6395806c25d76215cc73 🚀

--- library.24b57cb5.js
+++
@@ -1,2 +0,0 @@
-var btn=document.querySelector("#queue");function onClick(){console.log("hello")}btn.addEventListener("click",onClick);
-//# sourceMappingURL=library.24b57cb5.js.map
---
+++ library.e58b4a67.js
@@ -0,0 +1,2 @@
+var id=[1,2,3,4];localStorage.setItem("id",JSON.stringify(id));var refs={btn:document.querySelector("#queue")};function btnRenderQueueList(){console.log(localStorage.getItem("id"))}refs.btn.addEventListener("click",btnRenderQueueList);
+//# sourceMappingURL=library.e58b4a67.js.map
