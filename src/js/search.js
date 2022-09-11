2f39f03fa76b70438019840a0749fda5e1d7eb31
Alexandra-Makarenko <37274881+Alexandra-Makarenko@users.noreply.github.com>
2022-09-11 17:13:51 +0300

    закоментувала

--- src/js/search.js
+++ src/js/search.js
@@ -1,21 +1,21 @@
-import { isQueryOrPopular } from './api-top-films';
+// import { isQueryOrPopular } from './api-top-films';
 
-const searchForm = document.querySelector('.header__search');
+// const searchForm = document.querySelector('.header__search');
 
-searchForm.addEventListener('submit', onSearch);
+// searchForm.addEventListener('submit', onSearch);
 
-export function onSearch(e) {
-  e.preventDefault();
+// export function onSearch(e) {
+//   e.preventDefault();
 
-  apiService.query = e.currentTarget.searchQuery.value.trim();
-  inputError.textContent = ' ';
+//   apiService.query = e.currentTarget.searchQuery.value.trim();
+//   inputError.textContent = ' ';
 
-  apiService.resetPage();
+//   apiService.resetPage();
 
-  if (!apiService.query) {
-    textContent = 'Please enter something to search ';
-    return;
-  }
+//   if (!apiService.query) {
+//     textContent = 'Please enter something to search ';
+//     return;
+//   }
 
-  isQueryOrPopular(apiService.query, apiService.page);
-}
+//   isQueryOrPopular(apiService.query, apiService.page);
+// }
