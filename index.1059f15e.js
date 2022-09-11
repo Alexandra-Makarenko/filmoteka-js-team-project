ca175db9d1a59b8b6187f1aea0bc2f9f81af9066
Alexandra-Makarenko <37274881+Alexandra-Makarenko@users.noreply.github.com>
2022-09-11 16:35:11 +0000

    Deploying to gh-pages from @ Alexandra-Makarenko/filmoteka-js-team-project@a67f368c539625585afba68fc790127464d8cc55 🚀

---
+++ index.1059f15e.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".card");e.addEventListener("click",(function(){e.classList.toggle("is-flipped")}));
+//# sourceMappingURL=index.1059f15e.js.map
---
+++ index.ec61e701.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".card");e.addEventListener("click",(function(){e.classList.toggle("is-flipped")}))}();
+//# sourceMappingURL=index.ec61e701.js.map
