5d314c54b04f355b9f78abf652ec5ea992192a35
Alexandra-Makarenko <37274881+Alexandra-Makarenko@users.noreply.github.com>
2022-09-11 17:11:53 +0000

    Deploying to gh-pages from @ Alexandra-Makarenko/filmoteka-js-team-project@07bad55e21eadb391c8550538c0ba730b7f33137 🚀

---
+++ library.6a4e0529.js
@@ -0,0 +1,2 @@
+localStorage.setItem("id",JSON.stringify([1,2,3,4])),document.querySelector("#queue").addEventListener("click",(function(){console.log(localStorage.getItem("id"))}));
+//# sourceMappingURL=library.6a4e0529.js.map
---
+++ library.a9ff7d37.js
@@ -0,0 +1,2 @@
+localStorage.setItem("id",JSON.stringify([1,2,3,4]));({btn:document.querySelector("#queue")}).btn.addEventListener("click",(function(){console.log(localStorage.getItem("id"))}));
+//# sourceMappingURL=library.a9ff7d37.js.map
