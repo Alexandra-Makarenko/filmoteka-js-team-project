8a3ba3ed58c5607ee332a53229c1bce128223e0d
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-09-11 13:36:29 +0300

    add genres and pages

--- src/js/fetchTopFilms.js
+++ src/js/fetchTopFilms.js
@@ -1,15 +1,17 @@
+import { API_KEY } from './api-key';
 export default class ApiTopFilms {
   constructor() {
     this.mediaType = 'movie';
     this.timeWindow = 'week';
   }
 
-  fetchTopFilms() {
-    const API_KEY = '88804fe82d069d316bec59240a5ee94b';
+  fetchTopFilms(page) {
     const BASE_URL = 'https://api.themoviedb.org/3/trending/';
     const axios = require('axios');
     return axios
-      .get(`${BASE_URL}${this.mediaType}/${this.timeWindow}?api_key=${API_KEY}`)
+      .get(
+        `${BASE_URL}${this.mediaType}/${this.timeWindow}?api_key=${API_KEY}&page=${page}`
+      )
       .then(function (r) {
         return r;
       });
