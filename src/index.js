45735529f61ab9a83fd93aac62c8fd04a4accd30
Alexandra-Makarenko <s850272@gmail.com>
2022-09-12 22:29:34 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import "./js/loader";
 import './sass/index.scss';
 import './js/api-key';
-import './js/search';
+// import './js/search';
 import './js/api-top-films';
 import './js/local-storage';
 import './js/pagination';
@@ -9,5 +10,8 @@ import './js/array-of-genres';
 import './js/fetchTopFilms';
 import './js/footer-modal';
 import './js/team-photo';
-import "./js/loader";
+import './js/movie-modal';
+import './js/modal-open';
+import './js/modal-close';
+
 
