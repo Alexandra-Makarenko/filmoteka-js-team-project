7a795480fc22c7d82736894b9d13b4623fa38f24
Alexandra-Makarenko <s850272@gmail.com>
2022-09-12 17:51:35 +0300

    search
    знаходить і малює перші 20 фільмів по слову з пошуку (поки тільки через інпут)

--- src/js/search.js
+++ src/js/search.js
@@ -5,15 +5,17 @@ import { API_KEY } from './api-key';
 import axios from 'axios';
 
 const BASE_URL = 'https://api.themoviedb.org/3/'; // нужно вынести ее в отдельный файл чтоб все могли экспортировать
-const searchQuery = document.querySelector('input[name="searchQuery"]');
+const searchQuery = document.querySelector('.search__input');
 
 let userSearchData = '';
 
-const searchBtn = document.querySelector('.input__search-btn');
+const filmList = document.querySelector('.film_list');
 
 const createUrlFilmSearch = () => {
-  userSearchData = searchQuery.value.replace(' ', '+');
+  userSearchData = searchQuery.value;
   const urlFilmSearch = `${BASE_URL}search/movie?api_key=${API_KEY}&query=${userSearchData}`;
+  
+  console.log(urlFilmSearch)
   return urlFilmSearch;
 };
 
@@ -25,10 +27,28 @@ const handleSearchSubmit = async event => {
     const urlFilmSearch = await createUrlFilmSearch();
     const response = await axios.get(urlFilmSearch);
     const { data } = response;
-    console.log(data);
-  } catch (error) {
+    console.log(data.results);
+    filmList.innerHTML = '';
+    data.results.map(film => {
+      const yearOfRelease = film.release_date.split('-');
+         filmList.insertAdjacentHTML("beforeend",`<li id="" class="film-list__item">
+    <img
+    src="https://image.tmdb.org/t/p/w500${film.poster_path}"
+    alt="Movie Name"
+    class="film-list__item-poster"
+    loading="lazy"
+    />
+    <div class="film-list__item-info">
+    <h3 class="film-list__item-title">${film.title}</h3>
+   <span class="film-list__item-genres">жанри</span>
+   <span class="film-list__item-genres">${yearOfRelease[0]}</span>
+    <span class="film-list__item-rate">${film.vote_average}</span>
+    </div>
+    </li>`)
+       })
+      } catch (error) {
     console.log(error);
   }
 };
 
-searchBtn.addEventListener('click', handleSearchSubmit);
+searchQuery.addEventListener('change', handleSearchSubmit);
