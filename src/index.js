e37906343d0953b8730a3dc3231f53da56595b30
Vasilina Biznyakova <vbiznyakova@gmail.com>
2022-09-12 14:43:14 +0200

    taged input + search button into <form > + add API request to search films by key words to file search.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import './sass/index.scss';
+// import './sass/index.scss';
+import './js/api-key';
 import './js/search';
-
 // import './js/pagination';
 // import './js/api-top-films';
-
--- src/js/search.js
+++ src/js/search.js
@@ -1,21 +1,34 @@
-// import { isQueryOrPopular } from './api-top-films';
+// ---------
+// в header-home.html need to add name="searchQuery" to input + add form instead of div
 
-// const searchForm = document.querySelector('.header__search');
+import { API_KEY } from './api-key';
+import axios from 'axios';
 
-// searchForm.addEventListener('submit', onSearch);
+const BASE_URL = 'https://api.themoviedb.org/3/'; // нужно вынести ее в отдельный файл чтоб все могли экспортировать
+const searchQuery = document.querySelector('input[name="searchQuery"]');
 
-// export function onSearch(e) {
-//   e.preventDefault();
+let userSearchData = '';
 
-//   apiService.query = e.currentTarget.searchQuery.value.trim();
-//   inputError.textContent = ' ';
+const searchBtn = document.querySelector('.input__search-btn');
 
-//   apiService.resetPage();
+const createUrlFilmSearch = () => {
+  userSearchData = searchQuery.value.replace(' ', '+');
+  const urlFilmSearch = `${BASE_URL}search/movie?api_key=${API_KEY}&query=${userSearchData}`;
+  return urlFilmSearch;
+};
 
-//   if (!apiService.query) {
-//     textContent = 'Please enter something to search ';
-//     return;
-//   }
+const handleSearchSubmit = async event => {
+  event.preventDefault();
 
-//   isQueryOrPopular(apiService.query, apiService.page);
-// }
+  // need to add - clearPreviousSearch();
+  try {
+    const urlFilmSearch = await createUrlFilmSearch();
+    const response = await axios.get(urlFilmSearch);
+    const { data } = response;
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+searchBtn.addEventListener('click', handleSearchSubmit);
