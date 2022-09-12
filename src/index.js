51e923923bdfa58b1ff544a65c5076cc23479e0e
helga-khols <helga.khols@gmail.com>
2022-09-12 23:51:03 +0300

    fix modal-open

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./js/loader";
+import './js/loader';
 import './sass/index.scss';
 import './js/api-key';
 // import './js/search';
@@ -10,8 +10,6 @@ import './js/array-of-genres';
 import './js/fetchTopFilms';
 import './js/footer-modal';
 import './js/team-photo';
-// import './js/movie-modal';
-// import './js/modal-open';
-// import './js/modal-close';
-
-
+import './js/movie-modal';
+import './js/modal-open';
+import './js/modal-close';
