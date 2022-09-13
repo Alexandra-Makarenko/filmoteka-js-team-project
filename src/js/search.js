d7c25f9e5db3cf5cf08cb6d6aafdd5e05372c6a2
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-09-13 16:22:25 +0300

    fixed search

--- src/js/search.js
+++ src/js/search.js
@@ -1,10 +1,8 @@
 // ---------
 // в header-home.html need to add name="searchQuery" to input + add form instead of div
-
+import Notiflix from 'notiflix';
 import { API_KEY } from './api-key';
 import axios from 'axios';
-import { patternOfCard } from './api-top-films';
-import { decipherGenresIds } from './api-top-films';
 import { rendOneCard } from './api-top-films';
 const BASE_URL = 'https://api.themoviedb.org/3/'; // нужно вынести ее в отдельный файл чтоб все могли экспортировать
 const searchQuery = document.querySelector('.search__input');
@@ -14,39 +12,39 @@ let userSearchData = '';
 
 const filmList = document.querySelector('.film_list');
 
-const createUrlFilmSearch = () => {
-  userSearchData = searchQuery.value;
-  const urlFilmSearch = `${BASE_URL}search/movie?api_key=${API_KEY}&query=${userSearchData}`;
+const createUrlFilmSearch = page => {
+  userSearchData = searchQuery.value.trim();
+  const urlFilmSearch = `${BASE_URL}search/movie?api_key=${API_KEY}&query=${userSearchData}&page=${page}`;
 
   console.log(urlFilmSearch);
   return urlFilmSearch;
 };
 
-function posterLinkGenerate(film) {
-  if (film.poster_path !== null) {
-    return `"https://image.tmdb.org/t/p/w500${film.poster_path}"`;
-  }
-  return `"https://raw.githubusercontent.com/Alexandra-Makarenko/filmoteka-js-team-project/main/src/images/no-poster.jpg"`;
-}
-
 const handleSearchSubmit = async event => {
   event.preventDefault();
-
-  // need to add - clearPreviousSearch();
   try {
-    const urlFilmSearch = await createUrlFilmSearch();
-    const response = await axios.get(urlFilmSearch);
-    const { data } = response;
-    console.log(data.results);
-    filmList.innerHTML = '';
-    await data.results.map(film => {
-      rendOneCard(film).then(r => {
-        filmList.insertAdjacentHTML('beforeend', r);
-      });
-    });
+    searchPages(2);
   } catch (error) {
     console.log(error);
   }
 };
+
+async function searchPages(page) {
+  const urlFilmSearch = await createUrlFilmSearch(page);
+  const response = await axios.get(urlFilmSearch);
+  const { data } = response;
+  if (data.results.length === 0) {
+    Notiflix.Notify.failure('Error! Search does not give result');
+    return;
+  }
+  Notiflix.Notify.success('Success search');
+  filmList.innerHTML = '';
+  await data.results.map(film => {
+    rendOneCard(film).then(r => {
+      filmList.insertAdjacentHTML('beforeend', r);
+    });
+  });
+  console.log(userSearchData);
+}
 
 formSearch.addEventListener('submit', handleSearchSubmit);
