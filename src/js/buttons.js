f66b42db81f6bdeee8e4b4455cf52b59675e1178
Victiriia Ripka <98872709+Victoriia-Ripka@users.noreply.github.com>
2022-09-13 21:39:59 +0300

    script for btn local storage

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,23 +1,12 @@
 import { API_KEY } from './api-key';
 import { arrayOfGenres } from './array-of-genres';
-//заглушка айдішніків поки нема локального збереження
-const id = [716037, 632639, 561743, 860741, 862504]; //заглушка для черги
-const idw = [16037, 532630, 617430, 760412, 76504]; //заглушка для переглянутого
-localStorage.setItem('watched', JSON.stringify(idw));
-localStorage.setItem('queue', JSON.stringify(id));
 
 const baseURL = 'https://api.themoviedb.org/3/movie/';
-const savedWatchedFilms = JSON.parse(
-  localStorage.getItem('watched').split(',')
-);
-const savedQueueFilms = JSON.parse(localStorage.getItem('queue').split(','));
 
 const refs = {
   btnQueue: document.querySelector('#queue'),
   btnWatched: document.querySelector('#watched'),
-  div: document.querySelector('#library'),
   text: document.querySelector('#text'),
-  btnWatched: document.querySelector('#watched'),
   filmList: document.querySelector('.film_list'),
 };
 
@@ -27,31 +16,57 @@ refs.btnWatched.addEventListener('click', onBtnWatched);
 const fetchQueueFilms = async id => {
   const response = await fetch(`${baseURL}${id}?api_key=${API_KEY}`);
   const films = await response.json();
-  // console.log(films);
   return films;
 };
-
-function posterLinkGenerate(film) {
-  if (film.poster_path !== null) {
-    return `"https://image.tmdb.org/t/p/w500${film.poster_path}"`;
-  }
-  return `"https://raw.githubusercontent.com/Alexandra-Makarenko/filmoteka-js-team-project/main/src/images/no-poster.jpg"`;
-}
 
 function onBtnQueue() {
   hideMessage();
   refs.filmList.innerHTML = '';
-  if (!savedQueueFilms) {
-    refs.filmList.innerHTML = '<p>Sorry, list is empty(</p>';
+  try{
+    const savedQueueFilms = JSON.parse(localStorage.getItem('queue').split(','));
+    savedQueueFilms.map(film =>
+      fetchQueueFilms(film).then(film => {
+        rendOneCard(film).then(r => {
+          refs.filmList.insertAdjacentHTML('beforeend', r);
+        });
+      })
+    );
+  } catch (error) {
+    refs.filmList.innerHTML = '<p class="library-message">Sorry, list is empty(</p>';
   }
+}
 
-  savedQueueFilms.map(film =>
-    fetchQueueFilms(film).then(film => {
+const fetchWatchedFilms = async idw => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${idw}?api_key=${API_KEY}`
+  );
+  const films = await response.json();
+  return films;
+};
+
+function onBtnWatched() {
+  hideMessage();
+  refs.filmList.innerHTML = '';
+  try{
+  const savedWatchedFilms = JSON.parse(localStorage.getItem('watched').split(','));
+  savedWatchedFilms.map(film =>
+    fetchWatchedFilms(film).then(film => {
       rendOneCard(film).then(r => {
         refs.filmList.insertAdjacentHTML('beforeend', r);
       });
     })
   );
+  } catch (error){
+    refs.filmList.innerHTML = '<p class="library-message">Sorry, list is empty(</p>';
+  }
+}
+
+//заховати повідомлення "будь ласка, оберіть категорію"
+function hideMessage(){
+  if(refs.text.classList.contains("library-message")){
+    refs.text.classList.remove('library-message')
+    refs.text.classList.add('library-message-hidden')
+  }
 }
 
 function rendOneCard(film) {
@@ -135,33 +150,9 @@ async function patternOfCard(film, genres, yearOfRelease) {
 </li>`;
 }
 
-//заховати повідомлення "будь ласка, оберіть категорію"
-function hideMessage(){
-  if(refs.text.classList.contains("library-message")){
-    refs.text.classList.remove('library-message')
-    refs.text.classList.add('library-message-hidden')
+function posterLinkGenerate(film) {
+  if (film.poster_path !== null) {
+    return `"https://image.tmdb.org/t/p/w500${film.poster_path}"`;
   }
-}
-
-const fetchWatchedFilms = async idw => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/movie/${idw}?api_key=${API_KEY}`
-  );
-  const films = await response.json();
-  return films;
-};
-
-function onBtnWatched() {
-  hideMessage();
-  refs.filmList.innerHTML = '';
-  if (!savedWatchedFilms) {
-    refs.filmList.innerHTML = '<p>Sorry, list is empty(</p>';
-  }
-  savedWatchedFilms.map(film =>
-    fetchWatchedFilms(film).then(film => {
-      rendOneCard(film).then(r => {
-        refs.filmList.insertAdjacentHTML('beforeend', r);
-      });
-    })
-  );
+  return `"https://raw.githubusercontent.com/Alexandra-Makarenko/filmoteka-js-team-project/main/src/images/no-poster.jpg"`;
 }
--- src/js/movie-modal.js
+++ src/js/movie-modal.js
@@ -2,6 +2,7 @@ import ApiTopFilms from './fetchTopFilms';
 import axios from 'axios';
 import { movieModal } from './modal-close';
 import { Notify } from 'notiflix';
+import { arrayOfGenres } from './array-of-genres'
 
 export const IMG_BASE_URL = 'https://image.tmdb.org/t/p/'; // Base URL to get movies dates
 export const IMG_FILE_SIZE = 'w500';
@@ -12,6 +13,20 @@ export async function fetchMovieById(id, imgSrc) {
     api_key: '88804fe82d069d316bec59240a5ee94b',
   };
   const response = await axios.get(`${url}?api_key=${parmams.api_key}`);
+
+  const ganresOfFilm = await arrayOfGenres.then(r => {
+    return r
+      .filter(genre => {
+        for (let id = 0; id<19; id++) { //const id of film.genre_ids
+          if (id === genre.id) {
+            return genre.name;
+          }
+        }
+      })
+      .map(genre => genre.name);
+  })
+  .then(r => [...r]);
+  console.log(ganresOfFilm)
 
   const watchedIdData = JSON.parse(localStorage.getItem('watched')) || [];
   const queuedIdData = JSON.parse(localStorage.getItem('queued')) || [];
@@ -42,7 +57,7 @@ export async function fetchMovieById(id, imgSrc) {
       <li class="movie-details--data-item original-title">${
         response.data.original_title
       }</li>
-      <li class="movie-details--data-item genre">жанри
+      <li class="movie-details--data-item genre">${ganresOfFilm}
   </li>
     </ul>
   </div>
@@ -76,18 +91,21 @@ export async function fetchMovieById(id, imgSrc) {
   }
 
   // add to local storage on click + add notifications + change text on btns + make the required btns disabled
+  let database;
 
   const onQueuedBtnClick = e => {
     if (!queuedIdData.includes(id)) {
       queuedIdData.push(id);
-      localStorage.setItem('queued', JSON.stringify(queuedIdData));
+      database = localStorage.setItem('queued', JSON.stringify(queuedIdData));
+      console.log(database)
       Notify.success('The film has been successfully added to Queued list');
       watchedBtn.disabled = true;
       queuedBtn.textContent = 'Being queued';
     } else {
       const queuedIdToRemove = queuedIdData.indexOf(id);
       queuedIdData.splice(queuedIdToRemove, 1);
-      localStorage.setItem('queue', JSON.stringify(queuedIdData));
+      database = localStorage.setItem('queue', JSON.stringify(queuedIdData));
+      console.log(database)
       Notify.warning('The film has been removed from Queued list');
       queuedBtn.textContent = 'add to queue';
     }
@@ -112,52 +130,3 @@ export async function fetchMovieById(id, imgSrc) {
   queuedBtn.addEventListener('click', onQueuedBtnClick);
   watchedBtn.addEventListener('click', onWatchedBtnClick);
 }
-
-// export function markupModalFilm(film, imgSrc) {
-//   console.log(film);
-//   console.log(film.budget);
-//   console.log(imgSrc);
-//   return `<div class="poster-img">
-//         <img src="${imgSrc}" alt="poster" />
-//       </div>
-//       <div class="movie-info">
-//         <h3 class="movie-title">${film.title}</h3>
-//         <div class="details-wrapper">
-//           <ul class="movie-details">
-//             <li class="movie-details--item">Vote / Votes</li>
-//             <li class="movie-details--item">Popularity</li>
-//             <li class="movie-details--item">Original Title</li>
-//             <li class="movie-details--item">Genre</li>
-//           </ul>
-//           <ul class="movie-details--data">
-//             <li class="movie-details--data-item">
-//               <span class="vote">${film.vote_average.toFixed(
-//                 1
-//               )}</span> / <span class="votes">${film.vote_count}</span>
-//             </li>
-//             <li class="movie-details--data-item popularity">${
-//               film.popularity
-//             }</li>
-//             <li class="movie-details--data-item original-title">${
-//               film.original_title
-//             }</li>
-//             <li class="movie-details--data-item genre">жанри</li>
-//           </ul>
-//         </div>
-//         <h3 class="movie-description">About</h3>
-//         <p class="movie-description__text">
-//         </p>
-//         <div class="movie-popup__buttons">
-//         <button type="button" class="movie-info__button--watched">
-//           add to Watched
-//         </button>
-//         <button type="button" class="movie-info__button--queued">
-//           add to queue
-//         </button>
-//         </div>
-//       </div>;`;
-// }
-
-// function renderModal(string) {
-//   movieModal.insertAdjacentHTML('beforeend', markupModalFilm);
-// }
