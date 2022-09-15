9d3078a5327bd69076db33852c99884256d6d78a
Oleksii Bykovskyi <bikovskij@ukr.net>
2022-09-15 12:02:38 +0300

    add svg modal

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -1,25 +1,13 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-modal-open]'),
-//     closeModalBtn: document.querySelector('[data-modal-close]'),
-//     modal: document.querySelector('[data-modal]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-
 const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+};
+
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal(e) {
+  e.preventDefault();
+  refs.modal.classList.toggle('is-hidden');
 }
-  
-refs.openModalBtn.addEventListener('click', toggleModal)
-refs.closeModalBtn.addEventListener('click', toggleModal)
-
-  function toggleModal(e) {
-    e.preventDefault();
-    refs.modal.classList.toggle('is-hidden');
-  }
-// })();
-
